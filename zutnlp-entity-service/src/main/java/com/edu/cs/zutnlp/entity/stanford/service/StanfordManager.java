f1a2636817940a8d71f6f1424202cfa84a87eb6b
Mr.Ly <934060690@qq.com>
2019-07-24 10:06:12 +0800

    Update Stanford EntityRelation YoudaoTranslate  2019-7-24 10:06:00

--- zutnlp-entity-service/src/main/java/com/edu/cs/zutnlp/entity/stanford/service/StanfordManager.java
+++ zutnlp-entity-service/src/main/java/com/edu/cs/zutnlp/entity/stanford/service/StanfordManager.java
@@ -2,13 +2,38 @@ package com.edu.cs.zutnlp.entity.stanford.service;
 
 import com.edu.cs.zutnlp.entity.base.service.GenericManager;
 import com.edu.cs.zutnlp.entity.stanford.domain.Stanford;
+import edu.stanford.nlp.pipeline.Annotation;
 
 public interface StanfordManager extends GenericManager<Stanford, Long> {
     /**
-     * 1. EntityRecognotion
+     * 1. Entity
      * 2. EntityRalation
      */
 
-    String getEntity(String pra);
-    String getRelation(String text);
+    /**
+     * getEntity
+     * 1. Enlish
+     * 2. Chinese
+     * @param text
+     * @return
+     */
+    String getEntityEN(String text);
+    String getEntityCN(String text);
+
+    /**
+     * getEntityRelation
+     * 1. Enlish
+     * 2. Chinese
+     * @param text
+     * @return
+     */
+    String getRelationEN(String text);
+    String getRelationCN(String text);
+
+    /**
+     * getAnnotation
+     * @param text
+     * @return
+     */
+    Annotation getAnnotation(String text);
 }
--- zutnlp-entity-web/src/main/java/com/edu/cs/zutnlp/entity/stanford/web/spring/controller/StanfordController.java
+++ zutnlp-entity-web/src/main/java/com/edu/cs/zutnlp/entity/stanford/web/spring/controller/StanfordController.java
@@ -3,6 +3,7 @@ package com.edu.cs.zutnlp.entity.stanford.web.spring.controller;
 import com.edu.cs.zutnlp.entity.base.web.spring.controller.GenericController;
 import com.edu.cs.zutnlp.entity.stanford.domain.Stanford;
 import com.edu.cs.zutnlp.entity.stanford.service.StanfordManager;
+import com.edu.cs.zutnlp.entity.translate.service.YoudaoManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +22,20 @@ import java.util.Map;
 @RequestMapping("/stanford")
 public class StanfordController extends GenericController<Stanford, Long, StanfordManager> {
 
-    StanfordManager stanfordManager;
     private Stanford stanford=new Stanford();
+
+    StanfordManager stanfordManager;
+    YoudaoManager youdaoManager;
+
     @Autowired
     public void setStanfordManager(StanfordManager stanfordManager) {
         this.stanfordManager = stanfordManager;
         this.manager = this.stanfordManager;
+    }
+
+    @Autowired
+    public void setYoudaoManager(YoudaoManager youdaoManager) {
+        this.youdaoManager = youdaoManager;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/main")
@@ -42,49 +51,47 @@ public class StanfordController extends GenericController<Stanford, Long, Stanfo
     }
 
     @ResponseBody
-    @RequestMapping(value="request" , produces = "application/json;charset=utf-8")
-    public String getMessage(@RequestBody Map<String, Object> map){
-        String text = (String)map.get("text");
-        System.out.println("text");
-        return "text";
-    }
-
-//    @ResponseBody
-//    @RequestMapping(value = "response", produces = "application/json;charset=utf-8")
-//    public Stanford response(HttpServletResponse response) {
-//        response.setHeader("Access-Control-Allow-Origin", "*");
-//        Stanford stanford = new Stanford();
-//        stanford.setText("hello!");
-//        return stanford;
-//    }
-
-    @ResponseBody
     @GetMapping(value = "getentity/{text}",produces = "application/json;charset=utf-8")
     public String getEntity(@PathVariable("text") String text) {
-        String str = this.manager.getEntity(text);
-        System.out.println("Entity: " + str);
-        return str;
+        String entity = null;
+        if (this.youdaoManager.isEnlish(text)) {
+            entity = this.manager.getEntityEN(text);
+        } else {
+            entity = this.manager.getEntityCN(text);
+        }
+        return entity;
     }
     @ResponseBody
     @GetMapping(value = "getrelation/{text}",produces = "application/json;charset=utf-8")
     public String getRelationWeb(@PathVariable("text") String text){
-        String str = this.manager.getRelation(text);
-        System.out.println("Relation:" + str);
-        return str;
+        String relation = null;
+        if (this.youdaoManager.isEnlish(text)) {
+            relation = this.manager.getRelationEN(text);
+        } else {
+            relation = this.manager.getRelationCN(text);
+        }
+        return relation;
     }
+
 
     @ResponseBody
     @PostMapping(value = "request", produces = "application/json;charset=utf-8")
     public String getText(@RequestBody Map<String, Object> map) {
         String text = (String) map.get("text");
-        String  result = this.manager.getRelation(text);
-        String resultEntity=this.manager.getEntity(text);
+        String entity = null;
+        String relation = null;
+        if (this.youdaoManager.isEnlish(text)) {
+            entity = this.manager.getEntityEN(text);
+            relation = this.manager.getRelationEN(text);
+        } else {
+            entity = this.manager.getEntityCN(text);
+            relation = this.manager.getRelationCN(text);
+        }
         stanford.setText(text);
-        stanford.setRelation(result);
-        stanford.setEntity(resultEntity);
+        stanford.setEntity(entity);
+        stanford.setRelation(relation);
         this.manager.save(stanford);
-        System.out.print("text : ");
-        System.out.println(text);
+        System.out.print("text : " + text + "\n=============================================================OK!\n");
         return text;
     }
 
@@ -94,8 +101,5 @@ public class StanfordController extends GenericController<Stanford, Long, Stanfo
         response.setHeader("Access-Control-Allow-Origin", "*");
         return stanford;
     }
-
-
-
 
 }
